30ea051602853f2598c6768d0b0da2ce9c102725
Jack <sherlingjack@gmail.com>
2022-08-01 17:14:57 -0400

    remove unused variable

--- src/gui/Human.java
+++ src/gui/Human.java
@@ -54,9 +54,7 @@ public class Human implements ActionListener {
 	}
 	
 	class GUIPanel extends JPanel {
-		
-		int[][] b;
-		
+				
 		public void clear(Graphics g) {
 		 	g.setColor(Color.WHITE);
 	    	g.fillRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
